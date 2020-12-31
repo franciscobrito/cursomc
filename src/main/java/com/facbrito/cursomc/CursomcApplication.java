e5436902d42af9dbbe5169a90d7ae969faa77b5d
Francisco Brito <francisco.brito.ac@gmail.com>
2020-12-31 17:30:06 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/facbrito/cursomc/CursomcApplication.java
+++ src/main/java/com/facbrito/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.facbrito.cursomc.domain.Cidade;
 import com.facbrito.cursomc.domain.Cliente;
 import com.facbrito.cursomc.domain.Endereco;
 import com.facbrito.cursomc.domain.Estado;
+import com.facbrito.cursomc.domain.ItemPedido;
 import com.facbrito.cursomc.domain.Pagamento;
 import com.facbrito.cursomc.domain.PagamentoComBoleto;
 import com.facbrito.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.facbrito.cursomc.repository.CidadeRepository;
 import com.facbrito.cursomc.repository.ClienteRepository;
 import com.facbrito.cursomc.repository.EnderecoRepository;
 import com.facbrito.cursomc.repository.EstadoRepository;
+import com.facbrito.cursomc.repository.ItemPedidoRepository;
 import com.facbrito.cursomc.repository.PagamentoRepository;
 import com.facbrito.cursomc.repository.PedidoRepository;
 import com.facbrito.cursomc.repository.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -119,6 +124,19 @@ public class CursomcApplication implements CommandLineRunner {
 				
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));	
 	}
 
 }
--- src/main/java/com/facbrito/cursomc/domain/Produto.java
+++ src/main/java/com/facbrito/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.facbrito.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,34 +13,44 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA",
-		joinColumns = @JoinColumn(name = "produto_id"),
-		inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
-	
-	public Produto() {}
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public Produto() {
+	}
 
 	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido itemPedido : itens) {
+			lista.add(itemPedido.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -73,6 +85,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -97,6 +117,5 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
